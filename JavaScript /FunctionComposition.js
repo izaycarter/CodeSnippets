86f0114a2cec0ec9de3c0a8172396cc7480e7f06
izaycarter <izay.carter0509@gmail.com>
2025-04-14 11:25:44 -0400

    composition function

---
+++ JavaScript /FunctionComposition.js
@@ -0,0 +1,23 @@
+/*
+Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+*/
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    if(functions.length === 0)
+        return result => result;
+
+    return functions.reduce((preFn,nextFn) =>{
+        return result => preFn(nextFn(result));
+    });
+};
